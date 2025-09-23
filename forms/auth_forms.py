9868390f3a3fd4bb10fb09331cfcbeaa079eb1b2
UriCapdevila <Uri_CapdevilaWRK@outlook.com>
2025-09-23 12:01:44 -0300

    arreglos de codigo

--- forms/auth_forms.py
+++ forms/auth_forms.py
@@ -1,9 +1,13 @@
-# forms/auth_forms.py
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
-    username = StringField('Usuario', validators=[DataRequired()])
+    username = StringField('Usuario', validators=[DataRequired(), Length(min=3, max=30)])
     password = PasswordField('Contraseña', validators=[DataRequired()])
     submit = SubmitField('Ingresar')
+
+class RegistroForm(FlaskForm):
+    username = StringField('Usuario', validators=[DataRequired(), Length(min=3, max=30)])
+    password = PasswordField('Contraseña', validators=[DataRequired(), Length(min=6)])
+    submit = SubmitField('Registrarse')
